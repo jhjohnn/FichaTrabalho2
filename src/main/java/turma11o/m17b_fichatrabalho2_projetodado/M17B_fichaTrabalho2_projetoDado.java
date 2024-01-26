369589fa9167230c739f5b178aef01149a0c179d
John Gonçalves <>
2024-01-26 10:58:37 +0000

    mensagem comite

---
+++ src/main/java/turma11o/m17b_fichatrabalho2_projetodado/M17B_fichaTrabalho2_projetoDado.java
@@ -0,0 +1,32 @@
+// --- PSI M17B - projeto para a ficha de trabalho #2
+// O que é necessário fazer:
+// - criar um repositório no GitHub – sem README e tem de ser público!
+// - dar os passos necessários para fazer o Push do projeto no NetBeans para o 
+//   repositório criado anteriormente no GitHub.
+// - convidar o utilizador Nandoali e outro/a colega à vossa escolha para
+//   serem colaboradores do repositório; todos têm de aceitar os convites.
+// - publicar no fórum específico (Moodle) deste trabalho autónomo o link para
+//   o repositório.
+
+package turma11o.m17b_fichatrabalho2_projetodado;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Joao Barbosa
+ */
+public class M17B_fichaTrabalho2_projetoDado {
+
+    public static void main(String[] args) {
+        // Nota: este projeto tem uma outra classe, Janela, que 
+        // é executável.
+        // O código nesta classe limita-se a mostrar uma mensagem.
+        
+        JOptionPane.showMessageDialog(null,
+            "Sou o projeto dado.",
+            "Ficha de Trabalho #2",
+            JOptionPane.INFORMATION_MESSAGE);
+        
+    }
+}
